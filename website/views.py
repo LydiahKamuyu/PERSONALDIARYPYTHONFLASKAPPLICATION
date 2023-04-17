cfafb5aa9a5aa697edce92132c1ec84a6d951014
LydiahKamuyu <kamuyulydia@gmail.com>
2023-04-17 15:29:52 +0300

    Fixed the error of saving the data again when the page is reloadedeverytime

--- website/views.py
+++ website/views.py
@@ -1,9 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from .models import PersonalDiary
 from . import db
 from datetime import datetime
-from datetime import date
 
 views = Blueprint('views', __name__)
 
@@ -12,25 +11,24 @@ views = Blueprint('views', __name__)
 def home():
 	# Set up a default diary entry
 	default_entry = {'title': 'My first entry', 'date': datetime.now().strftime('%Y-%m-%d'), 'body': 'This is my first diary entry'}
-	
 	if request.method == 'POST':
 		# Get the form data
 		form_title = request.form.get('title')
-		form_date = request.form.get('today')
 		form_body = request.form.get('body')
 
 	#checks if the length of either form_title or form_body is less than or equal to 1.
-	if len(form_title) <= 1 or len(form_body) <= 1:
+		if len(form_title) <= 1 or len(form_body) <= 1:
 			flash('To short', category='error')
-	else:
+		else:
 
-		# Create a new diary entry with the form data
-		new_entry = PersonalDiary(title=form_title, body=form_body, date=datetime.now(), user_id=current_user.id)
-		
-		# Add the new entry to the database and display a success message
-		db.session.add(new_entry)
-		db.session.commit()
-		flash('Successfully Saved', category='success')
+			# Create a new diary entry with the form data
+			new_entry = PersonalDiary(title=form_title, body=form_body, date=datetime.now(), user_id=current_user.id)
+			
+			# Add the new entry to the database and display a success message
+			db.session.add(new_entry)
+			db.session.commit()
+			flash('Successfully Saved', category='success')
+			return redirect(url_for('views.home'))
 
 	# Get all diary entries for the current user
 	diary_entries = PersonalDiary.query.filter_by(user_id=current_user.id).all()
@@ -42,6 +40,5 @@ def home():
 
 	# Combine the default entry with the user's entries
 	all_entries = [default_entry] + diary_entries
-	today = date.today()
 
-	return render_template("home.html", user=current_user, diary_entries=diary_entries, date=today)
+	return render_template("home.html", user=current_user, diary_entries=diary_entries)
